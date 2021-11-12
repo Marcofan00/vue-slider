ebd32abb5e2aceb76deae7b5e92fa94c77319889
Marco <marcofan00@gmail.com>
2021-11-12 01:17:09 +0100

    fine ex

---
+++ js/script.js
@@ -0,0 +1,51 @@
+var app = new Vue({
+    // Creo il collegamento con l'html
+    el: '#app',
+    data: {
+        activeSlides: 0,
+        slides: [
+            {
+                image: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+            {
+                image: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+            {
+                image: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+            {
+                image: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+            {
+                image: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            }
+        ]
+    },
+    // Proprietà methods che determina lo scorrimento delle img
+    methods: {
+        btnNext() {
+            if (this.activeSlides < this.slides.length - 1) {
+                this.activeSlides++;
+            } else {
+                this.activeSlides = 0;
+            }
+        },
+        btnPrev() {
+            if (this.activeSlides === 0) {
+                this.activeSlides = this.slides.length - 1;
+            } else {
+                this.activeSlides--;
+            }
+        }
+    }
+});
